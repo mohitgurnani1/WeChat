530bdfdc833a523dc38de39b5ea8fa2cc5a9005c
mohitgurnani1 <mohitgurnani1@gmail.com>
2014-05-18 14:57:43 +0530

    Latest

--- src/com/example/wechat/ClientActivity.java
+++ src/com/example/wechat/ClientActivity.java
@@ -16,21 +16,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 /**
- * This is a simple Android mobile client
- * This application read any string massage typed on the text field and 
- * send it to the server when the Send button is pressed
- * Author by Lak J Comspace
- *
+This Client sends text message to the server
  */
 public class ClientActivity extends Activity {
-
+    static boolean flag=false;
 	private Socket client;
 	private PrintWriter printwriter;
-	private EditText textField;
+	private EditText textField;            //Accepting the text
 	private Button button;
-	private String messsage;
-    StringBuffer br=new StringBuffer();
-	TextView textFieldScreen;
+	private String messsage;               //send text message
+    StringBuffer br=new StringBuffer();  //For Appending purpose
+	TextView textFieldScreen;			//To Display the text Panel
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,39 +39,55 @@ public class ClientActivity extends Activity {
 		button.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
+				flag=false;                       //flag is used to check whether message is delivered or not,depending on that * will be appended along with text Message
 				textFieldScreen.setText("");
 				messsage = textField.getText().toString(); // get the text message on the text field
-				br.append("\n"+messsage);
+				br.append("\n"+messsage);  //Appending the message
 				textField.setText(""); // Reset the text field to blank
 				SendMessage sendMessageTask = new SendMessage();
-				sendMessageTask.execute();
-				textFieldScreen.setText(br.toString());
-			}
+				sendMessageTask.execute();    //Instead of using threads and handlers,Asyn Task is used to handle them.Its executed by object.execute();
+			    System.out.println("client="+flag);
+				flag=sendMessageTask.onPostExecute(); //To get final result				
+			    if(flag==true)
+				textFieldScreen.setText(br.toString()+"  *"); //Message Delivered
+			    else
+			    textFieldScreen.setText(br.toString());  //Message not delivered
+				   	
+			    
+			    }
 		});
 	}
 
 	private class SendMessage extends AsyncTask<Void, Void, Void> {
-
+boolean flag=false;
 		@Override
 		protected Void doInBackground(Void... params) {
 			try {
 
-				client = new Socket("192.168.0.4",5577); // connect to the server
-				printwriter = new PrintWriter(client.getOutputStream(), true);
-				printwriter.write(messsage); // write the message to output stream
-
+				client = new Socket("192.168.0.4",5578); // connect to the server: wifi ipv4 address
+			printwriter = new PrintWriter(client.getOutputStream(), true);
+			flag=true;
+			printwriter.write(messsage); // write the message to output stream
+System.out.println("send mesage="+ClientActivity.flag); //debugging
 				printwriter.flush();
 				printwriter.close();
 				client.close(); // closing the connection
-
+           
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
+				flag=false;
 			} catch (IOException e) {
 				e.printStackTrace();
+				flag=false;
 			}
 			return null;
 		}
 
+		
+		
+		 public boolean onPostExecute(Void... params) {
+		     return flag;
+		 }
 	}
 
 	@Override
